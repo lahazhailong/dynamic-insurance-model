0ecb0e0017fb8b33ccbe5ed7da06e838b10892c6
CarsonZhai <long.zhai@siebresystems.com>
2017-02-14 15:32:45 +0800

    Commit Application

---
+++ src/main/java/com/marsteam/Application.java
@@ -0,0 +1,17 @@
+package com.marsteam;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Carson on 2017/2/14.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
